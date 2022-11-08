528465b88264216db694fb3c608b77491546e485
Devimax <mccookdeveloper@gmail.com>
2022-11-08 22:05:33 +0300

    Coroutines START

---
+++ app/src/main/java/com/example/coroutinestart/MainActivity.kt
@@ -0,0 +1,63 @@
+package com.example.coroutinestart
+
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.util.Log
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.isVisible
+import com.example.coroutinestart.databinding.ActivityMainBinding
+import kotlin.concurrent.thread
+
+
+class MainActivity : AppCompatActivity() {
+
+    private val binding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+        binding.buttonLoad.setOnClickListener {
+            loadData()
+        }
+    }
+
+    private fun loadData() {
+        Log.d("MainActivity", "Load started: $this")
+        binding.progress.isVisible = true
+        binding.buttonLoad.isEnabled = false
+        loadCity {
+            binding.tvLocation.text = it
+            loadTemperature(it) {
+                binding.tvTemperature.text = it.toString()
+                binding.progress.isVisible = false
+                binding.buttonLoad.isEnabled = true
+                Log.d("MainActivity", "Load finished: $this")
+            }
+        }
+    }
+
+    private fun loadCity(callback: (String) -> Unit) {
+        thread {
+            Thread.sleep(5000)
+            runOnUiThread() { callback.invoke("Moscow") }
+        }
+    }
+
+    private fun loadTemperature(city: String, callback: (Int) -> Unit) {
+        thread {
+            runOnUiThread() {
+                Toast.makeText(
+                    this,
+                    getString(R.string.load_temperature_toast),
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+            Thread.sleep(5000)
+            runOnUiThread() { callback.invoke(17) }
+        }
+    }
+}
